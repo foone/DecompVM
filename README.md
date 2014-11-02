be031fd0be0467fe64e9b22b0cd6873a270038c9
Foone Turing <fooneturing@gmail.com>
2014-11-02 05:01:08 -0800

    Adding support for extracting from the demo EXE

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This is the replacement for DecompProxy, which is Windows-only. DecompVM runs an
 Steps to use:
 ==========
 
-1. Get the 3DMOVIE.EXE file from a copy of Microsoft 3D Movie Maker 
+1. Get the 3DMOVIE.EXE file from a copy of Microsoft 3D Movie Maker, or 3DMTRIAL.EXE from [the demo](ftp://ftp.microsoft.com/deskapps/kids/3dmm.exe).
 2. Get objdump(/.exe) from GNU bintools.
 3. Run extract.py to generate Decompress(1/2).s
 4. Run python decompress.py input.bin output.bin
@@ -18,7 +18,7 @@ TODO:
 
 * Document what tiny subset of x86 this VM supports
 * Optimize the VM. Large decompressions are slow (300k=5s)
-* Test if the free demo has the decompression functions and make extract.py support them if so.
+* ~~Test if the free demo has the decompression functions and make extract.py support them if so~~
 
 Credits & License:
 ==================
--- extract.py
+++ extract.py
@@ -1,35 +1,49 @@
 #!/usr/bin/python
 from subprocess import check_output
-import re
+import re,os,sys
 
 END_OF_FUNC_RE = re.compile('^\s+([0-9a-f]+):\s*(?:cc)?\s+int3$',re.I)
-EXE_PATH='3dmovie.exe'
-FUNCTIONS=[
-        {'start':'0x0434C00','name':'Decompress1'},
-        {'start':'0x0438910','name':'Decompress2'},
-]
+EXES={
+	'3dmovie.exe':[
+		{'start':'0x0434C00','name':'Decompress1'},
+		{'start':'0x0438910','name':'Decompress2'},
+	],
+	'3DMTRIAL.EXE':[
+		{'start':'0x0434C30','name':'Decompress1'},
+		{'start':'0x0438940','name':'Decompress2'},
+	]
+}
 
 def extractFunction(exe_path, offset):
-    objdump=['objdump','-Mintel','-b','pe-i386','-d',exe_path,
-            '--start-address={}'.format(offset),
-            '-wz']
-    ret=check_output(objdump)
-    seen_header = -1
-    for line in ret.splitlines():
-        line=line.rstrip()
-        if 'Disassembly of section .text' in line:
-            seen_header = 2 # skip two lines
-            continue
-        if seen_header > 0:
-            seen_header-=1
-        elif seen_header==0:
-            if END_OF_FUNC_RE.match(line):
-                return # we ran into the end of the function
-            yield line
+		objdump=['objdump','-Mintel','-b','pe-i386','-d',exe_path,
+						'--start-address={}'.format(offset),
+						'-wz']
+		ret=check_output(objdump)
+		seen_header = -1
+		for line in ret.splitlines():
+				line=line.rstrip()
+				if 'Disassembly of section .text' in line:
+						seen_header = 2 # skip two lines
+						continue
+				if seen_header > 0:
+						seen_header-=1
+				elif seen_header==0:
+						if END_OF_FUNC_RE.match(line):
+								return # we ran into the end of the function
+						yield line
 
 
 if __name__=='__main__':
-    for func in FUNCTIONS:
-        with open(func['name']+'.s','w') as f:
-            for line in extractFunction(EXE_PATH, func['start']):
-                print >>f, line
+	found_path = None
+	for exe_path in EXES:
+		if os.path.exists(exe_path):
+			found_path = exe_path
+			break
+	if found_path is None:
+		print 'No acceptable EXE files found'
+		sys.exit()
+
+	for func in EXES[found_path]:
+		with open(func['name']+'.s','w') as f:
+			for line in extractFunction(found_path, func['start']):
+				print >>f, line
